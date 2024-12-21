f98879d7dab40060a4ddca67425b6a4bb2fb3863
ibu <outsideworkibrahim@gmail.com>
2024-12-22 03:24:57 +0600

    rddf

--- routes/scoreRoute.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { sendScoreToDB } from '../controllers/scoreController.js'; 
-
-const router = express.Router();
-
-router.post('/sendScoreToDB', sendScoreToDB);
-export default router;
